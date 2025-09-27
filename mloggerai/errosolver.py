5b01e3d7ac84f9ae96d5d0115774a32e86d9e773
perronemirko <64288756+perronemirko@users.noreply.github.com>
2025-09-27 19:45:26 +0200

    Updating README.md New env variable to set up custom prompt to handle the error

--- mloggerai/errosolver.py
+++ mloggerai/errosolver.py
@@ -8,13 +8,20 @@ from openai.types.chat import ChatCompletionMessageParam
 from dotenv import load_dotenv
 import os
 
+
 class ErrorSolver:
-    def __init__(self, model=None, log_file="logs/logger.log", log_level=logging.DEBUG, output_language="italiano"):
-        
+    def __init__(
+        self,
+        model=None,
+        log_file="logs/logger.log",
+        log_level=logging.DEBUG,
+        output_language="italiano",
+    ):
+
         load_dotenv()  # Carica le variabili da .env
-        base_url:str = os.getenv("OPENAI_API_URL", "http://localhost:1234/v1")
-        api_key:str = os.getenv("OPENAI_API_KEY", "")
-        self.model:str = os.getenv("OPENAI_API_MODEL", None)
+        base_url: str = os.getenv("OPENAI_API_URL", "http://localhost:1234/v1")
+        api_key: str = os.getenv("OPENAI_API_KEY", "")
+        self.model: str = os.getenv("OPENAI_API_MODEL", None)
         if model is not None:
             self.model = model
         self.client = OpenAI(base_url=base_url, api_key=api_key)
@@ -42,19 +49,19 @@ class ErrorSolver:
 
     def solve_from_log(self, text):
         try:
+            system_prompt: str = os.getenv(
+                "OPENAI_API_PROMPT",
+                f"Trova il bug e proponi la soluzione in modo molto conciso.",
+            )
             messages = cast(
                 list[ChatCompletionMessageParam],
                 [
-                    {"role": "system",
-                     "content": f"Trova il bug e proponi la soluzione in modo molto conciso rispondendo sempre in lingua {self.output_language}"},
+                    {"role": "system", "content": f"{system_prompt}\nRispondi sempre in lingua {self.output_language}"},
                     {"role": "user", "content": text},
-                ]
+                ],
             )
             completion = self.client.chat.completions.create(
-                model=self.model,
-                temperature=0.3,
-                max_tokens=150,
-                messages=messages
+                model=self.model, temperature=0.3, max_tokens=150, messages=messages
             )
             return completion.choices[0].message.content.strip()
         except Exception as e:
@@ -73,9 +80,13 @@ class ErrorSolver:
                     combined = f"📘 Soluzione AI: {solution}"
                     solver.logger.debug(combined, extra={"_from_ai_solver": True})
                 except Exception as e:
-                    solver.logger.debug(f"Errore AI interno: {e}", extra={"_from_ai_solver": True})
+                    solver.logger.debug(
+                        f"Errore AI interno: {e}", extra={"_from_ai_solver": True}
+                    )
 
         handler = AIHandler()
         handler.setLevel(ai_level)
-        handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+        handler.setFormatter(
+            logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        )
         self.logger.addHandler(handler)
